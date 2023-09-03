5a855c5ca2acbc08202a1c7e9732a5f0102d2f46
Ralph Kevin Rynard E. Macahipay <53963085+ralphkevinmacahipay@users.noreply.github.com>
2023-09-03 16:53:33 +0800

    init_done_sale

--- lib/mobile/mobile_sale/mobile_sale.dart
+++ lib/mobile/mobile_sale/mobile_sale.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/configuration/icons.dart';
 import 'package:flutter_test_app/configuration/style.dart';
 import 'package:get/get.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -11,8 +12,8 @@ class MobileSaleWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: kGrey,
-      height: context.height,
+      color: kWhite,
+      height: context.percentHeight * 128.9408, // adjust if scroll is apply
       width: context.width,
       child: ListView(
         children: [
@@ -30,6 +31,64 @@ class MobileSaleWidget extends StatelessWidget {
                   color: kLightRed, fontSize: context.percentWidth * 9.33),
             ),
           ),
+          SizedBox(
+            height: context.percentHeight * 126,
+            child: GridView.builder(
+              itemCount: 6,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: context.percentHeight * 2.95,
+                  childAspectRatio: 1 / 1.6,
+                  crossAxisCount: 2),
+              itemBuilder: (context, index) => SizedBox(
+                  width: context.percentWidth * 44,
+                  height: context.percentHeight * 10.9,
+                  child: Stack(
+                    children: [
+                      // image
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: SizedBox(
+                          width: context.percentWidth * 37.6,
+                          height: context.percentWidth * 53.33,
+                          child: Image.asset(
+                            kSampleJacket,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          color: kLigthBrown,
+                          child: Text(
+                            "15% OFF",
+                            style: kInterMediumBold.copyWith(
+                                color: kWhite,
+                                fontSize: context.percentWidth * 2.66),
+                          ).paddingSymmetric(
+                              horizontal: context.percentWidth * 1.5,
+                              vertical: context.percentWidth * 1.5),
+                        ),
+                      ).marginOnly(top: context.percentHeight * 0.98),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "LOREM IPSUM",
+                              style: kInterSemiBold.copyWith(
+                                  fontSize: context.percentWidth * 4),
+                            ),
+                            Text(
+                              "Lorem ipsum",
+                              style: kInterRegurlar.copyWith(
+                                  fontSize: context.percentWidth * 3.46),
+                            )
+                          ])
+                    ],
+                  )),
+            ).paddingSymmetric(horizontal: context.percentWidth * 9.6),
+          )
         ],
       ),
     );
