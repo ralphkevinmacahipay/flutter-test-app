3aaf019a199344b490340e0574bec364e6d0b84f
Ralph Kevin Rynard E. Macahipay <53963085+ralphkevinmacahipay@users.noreply.github.com>
2023-09-04 04:54:55 +0800

    fix_scroll

--- lib/mobile/mobile_product/mobile_product.dart
+++ lib/mobile/mobile_product/mobile_product.dart
@@ -16,38 +16,40 @@ class ProductMobileWidget extends StatelessWidget {
       color: kBackGroundColor,
       height: context.percentHeight * 134.97,
       width: context.width,
-      child: Column(
-              children: kMobilePro
-                  .map(
-                    (kProdItem) => SizedBox(
-                      height: context.percentHeight * 36.945,
-                      child: Stack(
-                        children: [
-                          Align(
-                            alignment: Alignment.center,
-                            child: Image.asset(
-                              kProdItem['prod_item'],
-                              height: context.percentHeight * 36.945,
-                              width: context.percentWidth * 54.58,
-                              fit: BoxFit.fill,
+      child: SingleChildScrollView(
+        child: Column(
+                children: kMobilePro
+                    .map(
+                      (kProdItem) => SizedBox(
+                        height: context.percentHeight * 36.945,
+                        child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.center,
+                              child: Image.asset(
+                                kProdItem['prod_item'],
+                                height: context.percentHeight * 36.945,
+                                width: context.percentWidth * 54.58,
+                                fit: BoxFit.fill,
+                              ),
                             ),
-                          ),
-                          ElevatedWidgetButton(
-                                  kOnPress: () {},
-                                  kName: kProdItem['button_title'],
-                                  kAlignment: Alignment.bottomCenter,
-                                  kFontSize: context.percentWidth * 4,
-                                  kH: context.percentHeight * 3.200,
-                                  kW: context.percentWidth * 34.042,
-                                  kColor: kGrey)
-                              .paddingOnly(
-                                  bottom: context.percentHeight * 2.216)
-                        ],
-                      ),
-                    ).marginOnly(bottom: context.percentHeight * 6),
-                  )
-                  .toList())
-          .marginOnly(top: context.percentHeight * 6),
+                            ElevatedWidgetButton(
+                                    kOnPress: () {},
+                                    kName: kProdItem['button_title'],
+                                    kAlignment: Alignment.bottomCenter,
+                                    kFontSize: context.percentWidth * 4,
+                                    kH: context.percentHeight * 3.200,
+                                    kW: context.percentWidth * 34.042,
+                                    kColor: kGrey)
+                                .paddingOnly(
+                                    bottom: context.percentHeight * 2.216)
+                          ],
+                        ),
+                      ).marginOnly(bottom: context.percentHeight * 6),
+                    )
+                    .toList())
+            .marginOnly(top: context.percentHeight * 6),
+      ),
     );
   }
 }
--- lib/mobile/mobile_sale/mobile_sale.dart
+++ lib/mobile/mobile_sale/mobile_sale.dart
@@ -16,93 +16,95 @@ class MobileSaleWidget extends StatelessWidget {
       color: kWhite,
       height: context.percentHeight * 120, // adjust if scroll is apply
       width: context.width,
-      child: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(color: kWhite, boxShadow: [
-              BoxShadow(
-                  color: kBlack.withOpacity(.25),
-                  offset: const Offset(0, 4),
-                  blurRadius: 4)
-            ]),
-            child: Text(
-              "SALE",
-              textAlign: TextAlign.center,
-              style: kInterSemiBold.copyWith(
-                  color: kLightRed, fontSize: context.percentWidth * 9.33),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(color: kWhite, boxShadow: [
+                BoxShadow(
+                    color: kBlack.withOpacity(.25),
+                    offset: const Offset(0, 4),
+                    blurRadius: 4)
+              ]),
+              child: Text(
+                "SALE",
+                textAlign: TextAlign.center,
+                style: kInterSemiBold.copyWith(
+                    color: kLightRed, fontSize: context.percentWidth * 9.33),
+              ),
             ),
-          ),
-          SizedBox(
-            height:
-                context.percentHeight * 99.137, //context.percentHeight * 120,
-            child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 6,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  mainAxisSpacing: context.percentHeight * 2.95,
-                  childAspectRatio: 1 / 1.6,
-                  crossAxisCount: 2),
-              itemBuilder: (context, index) => SizedBox(
-                  width: context.percentWidth * 44,
-                  height: context.percentHeight * 10.9,
-                  child: Stack(
-                    children: [
-                      // image
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: SizedBox(
-                          width: context.percentWidth * 37.6,
-                          height: context.percentWidth * 53.33,
-                          child: Image.asset(
-                            kSampleJacket,
-                            fit: BoxFit.contain,
+            SizedBox(
+              height:
+                  context.percentHeight * 99.137, //context.percentHeight * 120,
+              child: GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 6,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    mainAxisSpacing: context.percentHeight * 2.95,
+                    childAspectRatio: 1 / 1.6,
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) => SizedBox(
+                    width: context.percentWidth * 44,
+                    height: context.percentHeight * 10.9,
+                    child: Stack(
+                      children: [
+                        // image
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: SizedBox(
+                            width: context.percentWidth * 37.6,
+                            height: context.percentWidth * 53.33,
+                            child: Image.asset(
+                              kSampleJacket,
+                              fit: BoxFit.contain,
+                            ),
                           ),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Container(
-                          color: kLigthBrown,
-                          child: Text(
-                            "15% OFF",
-                            style: kInterMediumBold.copyWith(
-                                color: kWhite,
-                                fontSize: context.percentWidth * 2.66),
-                          ).paddingSymmetric(
-                              horizontal: context.percentWidth * 1.5,
-                              vertical: context.percentWidth * 1.5),
-                        ),
-                      ).marginOnly(top: context.percentHeight * 0.98),
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              "LOREM IPSUM",
-                              style: kInterSemiBold.copyWith(
-                                  fontSize: context.percentWidth * 4),
-                            ),
-                            Text(
-                              "Lorem ipsum",
-                              style: kInterRegurlar.copyWith(
-                                  fontSize: context.percentWidth * 3.46),
-                            )
-                          ])
-                    ],
-                  )),
-            ).paddingSymmetric(horizontal: context.percentWidth * 9.6),
-          ).paddingOnly(top: context.percentHeight * 6.650),
-          ElevatedWidgetButton(
-                  kOnPress: () {},
-                  kName: "More",
-                  kAlignment: Alignment.center,
-                  kFontSize: context.percentWidth * 4.8,
-                  kH: context.percentHeight * 3.94,
-                  kW: context.percentWidth * 33.0,
-                  kColor: kWhite)
-              .paddingOnly(bottom: context.percentHeight * 5),
-        ],
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: Container(
+                            color: kLigthBrown,
+                            child: Text(
+                              "15% OFF",
+                              style: kInterMediumBold.copyWith(
+                                  color: kWhite,
+                                  fontSize: context.percentWidth * 2.66),
+                            ).paddingSymmetric(
+                                horizontal: context.percentWidth * 1.5,
+                                vertical: context.percentWidth * 1.5),
+                          ),
+                        ).marginOnly(top: context.percentHeight * 0.98),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "LOREM IPSUM",
+                                style: kInterSemiBold.copyWith(
+                                    fontSize: context.percentWidth * 4),
+                              ),
+                              Text(
+                                "Lorem ipsum",
+                                style: kInterRegurlar.copyWith(
+                                    fontSize: context.percentWidth * 3.46),
+                              )
+                            ])
+                      ],
+                    )),
+              ).paddingSymmetric(horizontal: context.percentWidth * 9.6),
+            ).paddingOnly(top: context.percentHeight * 6.650),
+            ElevatedWidgetButton(
+                    kOnPress: () {},
+                    kName: "More",
+                    kAlignment: Alignment.center,
+                    kFontSize: context.percentWidth * 4.8,
+                    kH: context.percentHeight * 3.94,
+                    kW: context.percentWidth * 33.0,
+                    kColor: kWhite)
+                .paddingOnly(bottom: context.percentHeight * 5),
+          ],
+        ),
       ),
     );
   }
